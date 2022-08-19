e0c1af7a067ce7e392def687862849ac2a4cd7e0
Allal EL BAKOURI <allal.elbakourii@gmail.com>
2022-08-19 19:22:52 +0100

    just a commit

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
 import Navbar from "./Navbar";
 import Hero from "./Hero";
 import Stats from "./Stats";
-// import Business from "./Business";
+import Business from "./Business";
 // import Billing from "./Billing";
 // import CardDeal from "./CardDeal";
 // import Clients from "./Clients";
