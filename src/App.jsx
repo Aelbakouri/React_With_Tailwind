a4edd92f1bef031ff482f8c02d4479a22396a352
Allal EL BAKOURI <allal.elbakourii@gmail.com>
2022-08-21 23:11:51 +0100

    just a commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import styles from "./style";
-import { Navbar,Billing, Hero, Stats, Business} from "./components";
+import { Navbar, Hero, Stats, Business} from "./components";
 
 const App = () => (
   <div className="bg-primary w-full overflow-hidden">
