e426871d58854b377fed59807890b83c875d2485
Allal EL BAKOURI <allal.elbakourii@gmail.com>
2022-08-23 22:39:56 +0100

    just a commit

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -12,7 +12,7 @@ const CardDeal = () => (
       <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
         Arcu tortor, purus in mattis at sed integer faucibus. Aliquet quis
         aliquet eget mauris tortor.ç Aliquet ultrices ac, ametau.
-      </p>
+      {/* </p> */}
 
       <Button styles={`mt-10`} />
     </div>
