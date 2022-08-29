33ee59a1255678613ea6e0ef1f6424af33b66e04
Allal EL BAKOURI <allal.elbakourii@gmail.com>
2022-08-29 16:43:54 +0100

    just a commit

--- src/components/index.js
+++ src/components/index.js
@@ -5,7 +5,7 @@ import Business from "./Business";
 import Billing from "./Billing";
 import CardDeal from "./CardDeal";
 import Clients from "./Clients";
-// import CTA from "./CTA";
+import CTA from "./CTA";
 // import Footer from "./Footer";
 // import Testimonials from "./Testimonials";
 
