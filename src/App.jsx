73fc4ff4735ef8537d115a14051fa6581acff682
Allal EL BAKOURI <allal.elbakourii@gmail.com>
2022-08-30 19:33:59 +0100

    just a commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import styles from "./style";
 import { Navbar,Billing, Hero, Stats, Business,CardDeal, Testimonials, Clients, CTA, Footer} from "./components";
 
 const App = () => (
-  
   <div className="bg-primary w-full overflow-hidden">
     <div className={`${styles.paddingX} ${styles.flexCenter}`}>
       <div className={`${styles.boxWidth}`}>
